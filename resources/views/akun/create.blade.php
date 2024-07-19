881bba62b30c41636e86054b6595ee32c9d73b8c
irchamzah <71950891+irchamzah@users.noreply.github.com>
2024-07-19 11:05:35 +0700

    Update create.blade.php

--- resources/views/akun/create.blade.php
+++ resources/views/akun/create.blade.php
@@ -16,19 +16,22 @@
         @csrf
         <div class="form-group">
             <label for="username">Username:</label>
-            <input type="text" class="form-control" id="username" name="username">
+            <input type="text" class="form-control" id="username" name="username" value="{{ old('username') }}">
         </div>
         <div class="form-group">
             <label for="password">Password:</label>
-            <input type="password" class="form-control" id="password" name="password">
+            <input type="password" class="form-control" id="password" name="password" value="{{ old('password') }}">
         </div>
         <div class="form-group">
             <label for="name">Name:</label>
-            <input type="text" class="form-control" id="name" name="name">
+            <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}">
         </div>
         <div class="form-group">
             <label for="role">Role:</label>
-            <input type="text" class="form-control" id="role" name="role">
+            <select class="form-control" id="role" name="role">
+                <option value="admin" {{ old('role')=='admin' ? 'selected' : '' }}>Admin</option>
+                <option value="author" {{ old('role')=='author' ? 'selected' : '' }}>Author</option>
+            </select>
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
